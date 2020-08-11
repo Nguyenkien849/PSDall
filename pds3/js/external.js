b30835b056a38af0bcb9bc85cbf6b9c65bf67c4b
unknown <anhkien8499@gmail.com>
2020-08-11 10:03:00 +0700

    first commit

---
+++ pds3/js/external.js
@@ -0,0 +1,88 @@
+
+let haha = document.getElementsByClassName('kkk');
+
+// Array.from(haha).forEach(function (value) {
+// 	console.log(value)
+// });
+
+
+// function lll(selector) {
+// 	let a = ++i;
+// 	selector.previousElementSibling.innerHTML = a;
+// }
+
+// function ppp(selector){
+// 	if (i == 0) {
+// 		return; // Chỗ này không cần return i == 0; mục đích khi i=0 thì ngừng xử lý nên chỉ cần return; hoặc return false; là đc
+// 	}
+// 	let a = --i;
+// 	selector.nextElementSibling.innerHTML = a;
+// }
+
+// let haha2 = document.getElementById('kkk2');
+
+// let k = 0;
+// function lll2() {
+
+// 	let a = ++k;
+
+// 	haha2.innerHTML = a;
+
+// }
+
+// function ppp2(){
+
+// 	if (k == 0) {
+// 		return i == 0;
+// 	}
+
+// 	let a = --k;
+
+// 	haha2.innerHTML = a;
+
+// }
+
+// let haha3 = document.getElementById('kkk3');
+// let l = 0;
+// function lll3() {
+
+// 	let a = ++l;
+
+// 	haha3.innerHTML = a;
+
+// }
+
+// function ppp3(){
+
+// 	if (l == 0) {
+// 		return l == 0;
+// 	}
+
+// 	let a = --l;
+
+// 	haha3.innerHTML = a;
+
+// }
+
+// let haha4 = document.getElementById('kkk4');
+// let m= 0;
+// function lll4() {
+
+// 	let a = ++m;
+
+// 	haha4.innerHTML = a;
+
+// }
+
+// function ppp4(){
+
+// 	if (m == 0) {
+// 		return m == 0;
+// 	}
+
+// 	let a = --m;
+
+// 	haha4.innerHTML = a;
+
+// }
+
